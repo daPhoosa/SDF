777bf40fc0ad30339633b9e1cb602b466190a4f8
Phillip Schmidt <pjs.schmidt@gmail.com>
2015-10-19 00:17:34 -0500

    Initial Commit

---
+++ SDF.cpp
@@ -0,0 +1,65 @@
+/* 
+ * SDF - Second Derivative Filter
+ * by Phillip Schmidt
+ * v1.0
+ *
+ */
+
+
+#include "SDF.h"
+
+
+// 
+SDF::SDF(float _AccelLimit, float _dt)
+{
+	
+	AccelLimit_dt = _AccelLimit * _dt;
+	AccelLimitInv_2 = 0.5f / _AccelLimit; // = 1 / (2 * A)		-- to reduce computations
+	
+	dt = _dt;
+
+	PositionNow = 0;
+	VelocityNow = 0;
+
+}
+
+// 
+float SDF::in(const float& target) // 78us on 16Mhz 
+{
+	float VelocityNext;
+	
+	// Predict future stopping point if deceleration starts now
+	if(VelocityNow > 0.0f)
+	{
+		VelocityNext   =  VelocityNow + AccelLimit_dt;
+		PositionFuture =  VelocityNext * VelocityNext * AccelLimitInv_2 + PositionNow;	// stopPosition = V^2 / (2 * A) + P
+	}
+	else
+	{
+		VelocityNext   =  VelocityNow - AccelLimit_dt;
+		PositionFuture = -VelocityNext * VelocityNext * AccelLimitInv_2 + PositionNow;
+	}
+	
+	// set acceleration direction based on predicted future stopping point
+	if(PositionFuture < target)
+	{
+		VelocityNow +=  AccelLimit_dt; // Integrate accel to get velocity
+	}
+	else
+	{
+		VelocityNow -= AccelLimit_dt;
+	}
+
+	// Integrate vel to get position
+	PositionNow += VelocityNow * dt;
+	
+	return PositionNow;
+	
+}
+
+
+float SDF::out()
+{
+	return PositionNow;
+}
+
---
+++ SDF.h
@@ -0,0 +1,32 @@
+/* 
+ * SDF.h
+ *
+ */
+
+
+#ifndef SDF_h
+#define SDF_h
+
+#include <arduino.h>
+
+class SDF
+{
+	public:
+	
+		SDF(float _AccelLimit, float _dt);
+		float in(const float& target);
+		float out();
+		
+
+	//private:
+	
+		float AccelLimit_dt;
+		float AccelLimitInv_2;
+		
+		float PositionNow, PositionFuture;
+		float VelocityNow;
+		float dt;
+
+};
+
+#endif
